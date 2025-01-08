00f233aa016ad965496608b292f63be7b52acd9c
David Okulski <32730627+DavidOkulski@users.noreply.github.com>
2025-01-08 09:41:14 -0800

    added db debigging

--- app/src/knexfile.js
+++ app/src/knexfile.js
@@ -11,13 +11,20 @@ module.exports = {
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
         port: process.env.POSTGRES_PORT,
-        pool: {
-            min: 2,
-            max: 10,
-            acquireTimeoutMillis: 60000,
-        },
-        acquireConnectionTimeout: 60000,
     },
+    pool: {
+        min: 2,
+        max: 10,
+        acquireTimeoutMillis: 60000,
+        afterCreate: (conn, done) => {
+            console.log(`[Knex] Connection created:`, conn);
+            done(null, conn);
+        },
+        destroy: (conn) => {
+            console.log(`[Knex] Connection destroyed:`, conn);
+        },
+    },
+    acquireConnectionTimeout: 60000,
     migrations: {
         directory: path.join(__dirname, "migrations"),
         pool: {
@@ -25,4 +32,33 @@ module.exports = {
             max: 2,
         },
     },
+    debug: true, // Enable SQL query debugging
+    log: {
+        warn(message) {
+            console.warn(`[Knex][WARN] ${message}`);
+        },
+        error(message) {
+            console.error(`[Knex][ERROR] ${message}`);
+        },
+        deprecate(message) {
+            console.warn(`[Knex][DEPRECATE] ${message}`);
+        },
+        debug(message) {
+            console.debug(`[Knex][DEBUG] ${message}`);
+        },
+    },
 };
+
+const knex = require('knex')(module.exports);
+
+knex.on('query', (query) => {
+    console.log('[Knex][QUERY] Executing:', query.sql);
+});
+
+knex.on('query-error', (error, obj) => {
+    console.error('[Knex][QUERY-ERROR]', error.message, obj);
+});
+
+knex.on('query-response', (response, query) => {
+    console.log('[Knex][QUERY-RESPONSE] Response:', response);
+});
