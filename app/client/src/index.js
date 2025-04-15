80011dc6072aadb3c3ca06d56e0094db96faeda8
David Okulski <32730627+DavidOkulski@users.noreply.github.com>
2025-04-15 09:42:28 -0700

    Removed logs

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -5,7 +5,7 @@ import { SSOProvider } from "@bcgov/citz-imb-sso-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log("Backend URL passed to SSOProvider:", process.env.REACT_APP_SSO_URL);
+//console.log("Backend URL passed to SSOProvider:", process.env.REACT_APP_SSO_URL);
 
 root.render(
   <React.StrictMode>
